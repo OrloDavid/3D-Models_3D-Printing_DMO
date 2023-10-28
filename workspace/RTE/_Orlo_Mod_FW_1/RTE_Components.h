ea50bbf33328cc56a62363d7011cee1bb575776b
DavidOrlo <david@davidorlo.com>
2023-10-28 13:28:42 -0400

    1up

---
+++ workspace/RTE/_Orlo_Mod_FW_1/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'anycubic' 
+ * Target:  'Orlo_Mod_FW_1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "HC32F460KETA.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
