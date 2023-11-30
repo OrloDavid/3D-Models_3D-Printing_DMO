70c28cb022ed806b8d4a1658732a37c4be516354
DavidOrlo <david@davidorlo.com>
2023-11-29 23:10:34 -0500

    Docu soon to follow

---
+++ Keil Tools/Get-GCC-Dirs.ps1
@@ -0,0 +1,46 @@
+﻿
+$prefix = "..\Source\" # Prefix if needed, i.e. c:\path_to_start or ..\Dir\
+param(
+    [string]$rootDirectory = "C:\path",
+    [string]$startFrom = "C:\path", # part of path to cut off from output and replace with prefix
+    [string]$outputFile = "C:\path\uvis-dirs.txt",
+    [string[]]$ignoreDirs = @() # Directories to ignore "c:\maindir\subdir,c:\maindir\anotherdir"
+)
+
+function Get-RelativeFolders {
+    param(
+        [string]$Path,
+        [string]$RelativeTo,
+        [string[]]$IgnorePaths
+    )
+
+    $relativeFolders = @()
+    Get-ChildItem -Path $Path -Recurse -Directory | ForEach-Object {
+        $fullPath = $_.FullName
+        $shouldIgnore = $IgnorePaths | Where-Object { $fullPath.StartsWith($_) }
+        if (-not $shouldIgnore) {
+            $relativePath = $fullPath -replace [regex]::Escape($RelativeTo), '$prefix'
+            $relativeFolders += $relativePath
+        }
+    }
+
+    return $relativeFolders
+}
+
+# Ensure the path ends with a backslash
+$rootDirectory = $rootDirectory.TrimEnd('\') + '\'
+$startFrom = $startFrom.TrimEnd('\') + '\'
+
+# Ensure all ignore directories end with a backslash
+$ignoreDirs = $ignoreDirs | ForEach-Object { $_.TrimEnd('\') + '\' }
+
+$relativeFolders = Get-RelativeFolders -Path $rootDirectory -RelativeTo $rootDirectory -IgnorePaths $ignoreDirs
+
+# Output with line breaks
+$relativeFolders -join "`r`n" | Out-File -FilePath $outputFile
+
+# Add 7 line breaks
+"`r`n`r`n`r`n`r`n`r`n`r`n`r`n" | Out-File -FilePath $outputFile -Append
+
+# Output without line breaks
+$relativeFolders -join ';' | Out-File -FilePath $outputFile -Append
